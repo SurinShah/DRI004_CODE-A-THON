51e66e959651f6779096591f652763d2db564113
StutiiiG <94684107+StutiiiG@users.noreply.github.com>
2023-02-19 13:05:40 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,10 +1,10 @@
-﻿<?php require('header.php'); 
+<?php require('header.php'); 
 ?>
         <br><br>
      
 <!-- STARTUP-->
-        <div class="row row-content align-items-center" style="background-color:     #bb99ff;">
-                   <div class=" col-12 col-md-7">
+        <div class="row row-content align-items-center" style="background-color: #556EE6; border-radius: 15px 50px 30px">
+                   <div class=" col-12 col-md-7" style="border-radius: 15px 50px 30px">
                                 <h2 class="mt-0">User Registration</h2>
                                 <p>
                                 <em>Affordable Policy options available!</em>
@@ -18,15 +18,15 @@
                         <div class="flip-card-front">
                             <!-- Front Code -->
                                 <!-- <div class="col-12 col-md-5"> -->
-                                    <img src="" class="d-flex ml-3 img-thumbnail align-self-center">
+                                    <img src="image/user2.jpg" class="d-flex ml-3 img-thumbnail align-self-center" style=  "border-radius: 15px 50px 30px">
                                <!--  </div> -->
                             
                         </div>
 
                         <div class="flip-card-back">
                             <!-- Back Code -->
-                            <a href="user.php?reg=signup" class="btn btn-primary">Sign Up</a>
-                            <a href="user.php?reg=login" class="btn btn-success">Log In</a>
+                            <a href="user.php?reg=signup" class="btn btn-primary" style ="border-radius: 25px">Sign Up</a>
+                            <a href="user.php?reg=login" class="btn btn-success" style ="border-radius: 25px">Log In</a>
 
                         </div>
                     </div>
@@ -36,7 +36,7 @@
         </div><br><br>
 
 <!-- INVESTOR DIVISION-->
-        <div class="row row-content align-items-center"  style="background-color: #ff8080;">
+        <div class="row row-content align-items-center"  style="background-color: #556EE6; border-radius: 15px 50px 30px">
             
                 <div class="col-12 col-md-7 order-md-last">
                     <h2 class="mt-0">Company Registration</h2>
@@ -52,18 +52,34 @@
                         <div class="flip-card-front">
                             <!-- Front Code -->
                                 <!-- <div class="col-12 col-md-5"> -->
-                                    <img src="" class="d-flex mr-3 img-thumbnail align-self-center">
+                                    <img src="image/user.jpg" class="d-flex mr-3 img-thumbnail align-self-center" style=  "border-radius: 15px 50px 30px">
                                <!--  </div> -->
                             
                         </div>
 
-                        <div class="flip-card-back" id="invflipback">
+                        <div class="flip-card-back" id="invflipback" style ="border-radius: 25px">
                             <!-- Back Code -->
-                        <a href="company.php?reg=signup" class="btn btn-primary">Sign Up</a>
-                        <a href="company.php?reg=login" class="btn btn-success">Log In</a>
+                        <a href="company.php?reg=signup" class="btn btn-primary" >Sign Up</a>
+                        <a href="company.php?reg=login" class="btn btn-success" style ="border-radius: 25px">Log In</a>
 
                         </div>
                     </div>
+                </div>
+
+                <!-- ************************ -->
+                
+        </div><br><br>
+
+        <div class="row row-content align-items-center" style="background-color: #556EE6;border-radius: 15px 50px 30px">
+                   <div class=" col-12 col-md-7">
+                    <h2 class="mt-0">Tutorial</h2>
+                   
+                </div>  
+                <video width="400" controls>
+                <source src="tutorial.mp4" type="video/mp4">
+                <source src="tutorial.ogg" type="video/ogg">
+                Your browser does not support video.
+                </video>
                 </div>
 
                 <!-- ************************ -->
